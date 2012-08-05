14e9ee4ddc537b4d12750b818ef1045481e675da
wito <witold.zegarowski@gmail.com>
2012-08-05 23:25:21 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-PirateBayWithFilmwebChromeExt
+PirateBay With Filmweb Chrome Extention
 =============================
 
-Chrome extension to Pirate Bay that integrates FilmWeb description of the movies
+Google Chrome Extension for PB site with following features:
+
+- removes adds
+- adds additional column to PB search results with links to:  Google search, Google graphic, Filmweb search for the original torrent title
+- adds additional column to PB search results with description and movie rating from FilmWeb ( it performs  some basic heuristic to extract movie title and perform ajax calls to get the data form filmweb)
+
--- js/pb.js
+++ js/pb.js
@@ -1,100 +1,98 @@
-var WITO = {
+// wito 2012
+function getTheFilms() {
+
+	$('iframe').remove()
+	$('#tableHead').children(":first")
+		.append("<th>Filmweb</th>")
+		.append("<th>Links</th>")
 	
-	getmyfilms: function(){
+	$('#searchResult').find("tbody").children().each(function(index) {
 	
-		$('iframe').remove();
-		$('#tableHead').children(":first").append("<th>Filmweb</th>");
-		$('#tableHead').children(":first").append("<th>Links</th>");
+		$(this)
+			.append("<td id=\"filmweb_"+index+"\"></td>")
+			.append("<td id=\"links_"+index+"\"></td>")
 		
-		$('#searchResult').find("tbody").children().each(function(index) {
+		original = $(this).find(" .detName").children(":first").html()
 		
-			$(this).append("<td id=\"filmweb_"+index+"\"></td>")
-			$(this).append("<td id=\"links_"+index+"\"></td>")
+		$('#searchResult').find("#links_"+index)						
+			.append("<a href='https://www.google.pl/search?"+$.param({q: original })+"' target='_blank'>[search]</a>")
+			.append("<br/>")
+			.append("<a href='https://www.google.pl/search?"+$.param({safe:"off", q: original, tbm:"isch"})+"' target='_blank'>[pics]</a>")
+			.append("<br/>")
+			.append("<a href='http://www.filmweb.pl/search?"+$.param({q: original }) +"' target='_blank'>[filmweb]</a>")
+		
+		possibleMovieTitle = false
+		var filmNameClean = original
+		year = filmNameClean.match(new RegExp("[\\(\\[\\ \\.\\*][1-2][0-9][0-9][0-9][\\)\\]\\ \\.\\*\\[]","gi"))
+		if (year!=null && year.length > 0) {
+			i = filmNameClean.indexOf(year[0])
+			filmNameClean = filmNameClean.substring(0, i)
+			possibleMovieTitle = true
+		}
+
+		filmNameClean = filmNameClean.replace(new RegExp("\\[.+\\]","gi"), "")
+		filmNameClean = filmNameClean.replace(new RegExp("\\(.+\\)","gi"), "")
+		
+		special = filmNameClean.match(new RegExp("SWEDISH|SWESUB|BDRIP|DVD|UNRATED|720p", "gi"))
+		if (special!=null && special.length > 0) {
+			i = filmNameClean.indexOf(special[0])
+			filmNameClean = filmNameClean.substring(0, i)
+			possibleMovieTitle = true
+		}
+
+		var toappnode = $('#searchResult').find("#filmweb_"+index)
+
+		if (!possibleMovieTitle) {
+			toappnode.append("not a movie ?")
+			return 
+		}
+
+		filmNameClean = filmNameClean.replace(new RegExp("[\\[\\]\\(\\)\\.\\-\\=]", "gi"), " ")
+
+		ttr = [
+			"XDM", "AAC", "HD", "CAM", "DVDScrRip", "Dvdscr", "Rip" , "1CD", "2CD", "MP3", "x264 5.1", "x264",
+			"dvd5", "DVDRip", "RRG", "Xvid", "ICTV", "NL subs", "IPS", "Rel -", "\\*", "720p", "Hindi", "-", "BRRip", 
+			"\\(Rel \\)", "\\( Rel \\)", "\\( \\)", "\\(\\)" ]
+		for (i in ttr) {
+			filmNameClean = filmNameClean.replace(new RegExp(ttr[i],"gi"), "")
+		}
+
+		filmNameClean = filmNameClean.replace(/^\s+|\s+$/g, "")
+		
+		if (filmNameClean=="") {
+			toappnode.append("not a movie ?")
+			return 
+		}
+
+		var imgUrl = chrome.extension.getURL("ajax_loading_small.gif")
+		toappnode.append("<img src=\""+imgUrl+"\" />")
+
+		$.ajax({
+		  url: "http://www.filmweb.pl/search?"+$.param({q: filmNameClean }),
+		  success: function(data) {
+
+			toappnode.empty()
 			
-			var original = $(this).find(" .detName").children(":first").html()
-			
-								
-			linksNode = $('#searchResult').find("#links_"+index)						
-			linksNode.append("<a href='https://www.google.pl/search?"+$.param({q: original })+"' target='_blank'>[search]</a>")
-			linksNode.append("<br/>")
-			linksNode.append("<a href='https://www.google.pl/search?"+$.param({safe:"off", q: original, tbm:"isch"})+"' target='_blank'>[pics]</a>")
-			linksNode.append("<br/>")
-			linksNode.append("<a href='http://www.filmweb.pl/search?"+$.param({q: original }) +"' target='_blank'>[filmweb]</a>")
-			
-			var filmNameClean = original
-			n = filmNameClean.indexOf("(");
-						
-			years = filmNameClean.match(new RegExp("[\\(\\[\\ \\.\\*][1-2][0-9][0-9][0-9][\\)\\]\\ \\.\\*\\[]","gi"))
-			if (years!=null && years.length > 0) {
-				n = filmNameClean.indexOf(years[0])
-				filmNameClean = filmNameClean.substring(0, n)
-			}else {
-				special = filmNameClean.match(new RegExp("SWEDISH|SWESUB|BDRIP|DVD|UNRATED|720p|\\(Movie\\)", "gi"))
+			firstFilm = $(data).find("#searchFixCheck").children(":first").find(".searchResultCol_2_wrapper")					
+			if (firstFilm.length == 0) {
+				toappnode.append("Can't find '"+filmNameClean+"'.")
+			} else {
+				firstFilm.find("a[href]").each(function() {
+					i = this.href.indexOf("/")
+					i = this.href.indexOf("/",i+1)
+					i = this.href.indexOf("/",i+1)
+					this.href = "http://www.filmweb.pl"+this.href.substring(i)
+				})
 				
-				if (special!=null && special.length > 0) {
-					n = filmNameClean.indexOf(special[0])
-					filmNameClean = filmNameClean.substring(0, n)
-				}else {
-					filmNameClean = undefined
-				}
+				toappnode.append(firstFilm)
 			}
-					
-			toappnode = $('#searchResult').find("#filmweb_"+index)					
-			if (filmNameClean != undefined) {			
-			
-				filmNameClean = filmNameClean.replace(new RegExp("[\\[\\]\\(\\)\\.\\-\\=]", "gi"), " ");
-				
-				ttr = ["XDM", "AAC", "HD", "CAM", "DVDScrRip", "Dvdscr", "Rip" , "1CD", "2CD", "MP3", "x264 5.1", "x264",
-				"dvd5", "DVDRip", "RRG", "Xvid", "ICTV", "NL subs", "Rel -", "\\*", "720p", 
-				"Hindi", "-", "BRRip", "\\(Rel \\)", "\\( Rel \\)", "\\( \\)", "\\(\\)"]
-				for (var i=0; i<ttr.length; i++) {
-					var patt=new RegExp(ttr[i],"gi");
-					filmNameClean = filmNameClean.replace(patt, "");
-				}				
-			
-				var imgUrl = chrome.extension.getURL("ajax_loading_small.gif");
-				toappnode.append("<img src=\""+imgUrl+"\" />")
-								
-			} else {				
-				toappnode.append("unparsable")				
-			}
-						
-			if (filmNameClean != undefined) {	
-			
-				searchUrl = "http://www.filmweb.pl/search?"+$.param({q: filmNameClean })
-	
-				$.ajax({
-				  url: searchUrl,
-				  success: function(data){
-				  				  				  
-					toappnode = $('#searchResult').find("#filmweb_"+index)
-					toappnode.empty()
-					
-					firstFilm = $(data).find("#searchFixCheck").children(":first").find(".searchResultCol_2_wrapper")					
-					if (firstFilm.length == 0) {
-						toappnode.append("Can't find '"+filmNameClean+"'.")
-					} else {
-						firstFilm.find("a[href]").each(function() {
-							n=0
-							n=this.href.indexOf("/",n+1);
-							n=this.href.indexOf("/",n+1);
-							n=this.href.indexOf("/",n+1);
-							this.href = "http://www.filmweb.pl"+this.href.substring(n)
-					   });
-						toappnode.append(firstFilm)
-					}
-					
-				
-				  }
-				});	
-			
-			}
-			
+		
+		  }
 		});	
-	}
+		
+	});	
 }
 
 $(document).ready(function(){
-	WITO.getmyfilms();
+	getTheFilms()
 });
-
